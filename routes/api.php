c057e1930c7089f56401ce1188f5418f27b00e22
Rolando Torres <ronnytorresmtz@gmail.com>
2020-07-25 10:43:02 -0500

    clean lines

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,6 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('comments', CommentController::class);
 
 Route::get('/comments', function(Request $request) {
-
     
     $showOnlyMyComments = ($request->showOnlyMyComments == "false") ? false : true;
 
